dddfc90231b086627a8098d68731d3d15e0aa717
Keshaw Krishna <keshawkrishna2007@outlook.com>
2024-06-02 20:44:57 +0530

    first commit

---
+++ src/main/java/org/example/Common/Commonfunction.java
@@ -0,0 +1,11 @@
+package org.example.Common;
+
+public class Commonfunction {
+
+    public void printArray(int[] arr){
+        System.out.println();
+        for(int n : arr){
+            System.out.print(n + " ");
+        }
+    }
+}
